089e9ee7d0b6c35cea014987a383e2ce56f07621
ParkSangSun1 <appleasz1028102@gmail.com>
2021-08-02 15:47:39 +0900

    [ADD] switchFragment

--- app/src/main/java/com/example/crc_android/SignUpActivity.kt
+++ app/src/main/java/com/example/crc_android/SignUpActivity.kt
@@ -2,8 +2,12 @@ package com.example.crc_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class SignUpActivity : AppCompatActivity() {
+
+    private var flag = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
@@ -13,5 +17,40 @@ class SignUpActivity : AppCompatActivity() {
         val transaction = supportFragmentManager.beginTransaction()
         transaction.add(R.id.sign_up_frame_layout, SignUpEmailFragment())
         transaction.commit()
+    }
+
+    fun nextBtnClick(view: View) {
+        switchFragment()
+    }
+
+    private fun switchFragment() {
+        if (flag in 0..3) {
+            val transaction = supportFragmentManager.beginTransaction()
+
+            when (flag) {
+                0 -> {
+                    transaction.setCustomAnimations(R.anim.right_in, R.anim.left_out)
+                        .replace(R.id.sign_up_frame_layout, SignUpEmailFragment())
+                    flag++
+                }
+                1 -> {
+                    transaction.setCustomAnimations(R.anim.right_in, R.anim.left_out)
+                        .add(R.id.sign_up_frame_layout, SignUpPasswordFragment())
+                    flag++
+                }
+                2 -> {
+                    transaction.setCustomAnimations(R.anim.right_in, R.anim.left_out)
+                        .add(R.id.sign_up_frame_layout, SignUpNameFragment())
+                    flag++
+                }
+                3 -> {
+                    transaction.setCustomAnimations(R.anim.right_in, R.anim.left_out)
+                        .add(R.id.sign_up_frame_layout, SignUpClassFragment())
+                    flag++
+                }
+            }
+            transaction.addToBackStack(null)
+            transaction.commit()
+        }
     }
 }
