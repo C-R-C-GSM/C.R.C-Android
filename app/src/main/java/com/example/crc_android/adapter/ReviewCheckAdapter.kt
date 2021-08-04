e71f36d570b83d54f8e5347cc50aa465607d1869
KyungMin <2004ckck@naver.com>
2021-08-04 23:50:53 +0900

    [CREATE] Review Adapter 생성

--- app/src/main/java/com/example/crc_android/adapter/ReviewCheckAdapter.kt
+++ app/src/main/java/com/example/crc_android/adapter/ReviewCheckAdapter.kt
@@ -1,20 +1,44 @@
 package com.example.crc_android.adapter
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.example.crc_android.R
+import com.example.crc_android.data.models.check.ReviewCheck
+import com.example.crc_android.databinding.ReviewItemRowBinding
 
 class ReviewCheckAdapter : RecyclerView.Adapter<ReviewCheckAdapter.ReviewCheckHolder>() {
-    class ReviewCheckHolder(val binding : ) : RecyclerView.ViewHolder() {
+    private val reviewList = mutableListOf<ReviewCheck>()
+
+    class ReviewCheckHolder(val binding: ReviewItemRowBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: ReviewCheck) {
+            binding.item = item
+            binding.executePendingBindings()
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): ReviewCheckHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding: ReviewItemRowBinding = DataBindingUtil
+                    .inflate(
+                        layoutInflater, R.layout.review_item_row,
+                        parent, false
+                    )
+                return ReviewCheckHolder(binding)
+            }
+        }
 
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ReviewCheckHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ReviewCheckHolder =
+        ReviewCheckHolder.from(parent)
 
-    }
 
     override fun onBindViewHolder(holder: ReviewCheckHolder, position: Int) {
+        holder.bind(reviewList[position])
     }
 
-    override fun getItemCount(): Int {
-    }
+    override fun getItemCount() = reviewList.size
 }
