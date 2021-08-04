c822b4d86cc370f1bf542ce3787de4b7bccbdc3c
KyungMin <2004ckck@naver.com>
2021-08-04 20:22:26 +0900

    [CREATE] ReviewCheckAdapter.kt 생성

---
+++ app/src/main/java/com/example/crc_android/adapter/ReviewCheckAdapter.kt
@@ -0,0 +1,20 @@
+package com.example.crc_android.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ReviewCheckAdapter : RecyclerView.Adapter<ReviewCheckAdapter.ReviewCheckHolder>() {
+    class ReviewCheckHolder(val binding : ) : RecyclerView.ViewHolder() {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ReviewCheckHolder {
+
+    }
+
+    override fun onBindViewHolder(holder: ReviewCheckHolder, position: Int) {
+    }
+
+    override fun getItemCount(): Int {
+    }
+}
