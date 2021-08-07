23f9a31ba32afdaaaa22259b452b50a2ae60b9f2
ParkSangSun1 <apple1028102@gmail.com>
2021-08-07 23:36:11 +0900

    [CLEAN]

--- app/src/main/java/com/example/crc_android/data/models/reviewRegister/ReviewRegister.kt
+++
@@ -1,8 +0,0 @@
-package com.example.crc_android.data.models.reviewRegister
-
-data class ReviewRegister(
-    val code: Int,
-    val `data`: List<Data>,
-    val message: String,
-    val success: Boolean
-)
