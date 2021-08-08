522970bcb540aa45eddae9d3b3226d2c1b1fbb74
SeungHyun <67039023+gusanans218@users.noreply.github.com>
2021-08-08 11:29:23 +0900

    ll

--- app/src/main/java/com/example/crc_android/registractionActivity.kt
+++ app/src/main/java/com/example/crc_android/registractionActivity.kt
@@ -3,6 +3,7 @@ package com.example.crc_android
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
 class registractionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
