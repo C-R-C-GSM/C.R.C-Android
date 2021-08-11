c362dd914f86d264681addafdcf311395f83a812
KyungMin <2004ckck@naver.com>
2021-08-12 00:31:07 +0900

    [CREATE] FriendDto

--- app/src/main/java/com/example/crc_android/data/models/FriendUserData.kt
+++ app/src/main/java/com/example/crc_android/data/models/FriendUserData.kt
@@ -2,7 +2,7 @@ package com.example.crc_android.data.models
 
 data class FriendUserData(
     val code: Int,
-    val `data`: List<Data>,
+    val data: List<Data>,
     val message: String,
     val success: Boolean
 )
