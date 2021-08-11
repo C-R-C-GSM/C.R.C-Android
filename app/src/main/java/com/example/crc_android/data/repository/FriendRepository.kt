0a2203f4c52127d0835480687c3170daeac779be
KyungMin <2004ckck@naver.com>
2021-08-12 00:30:19 +0900

    [CREATE] FriendRepository.kt

---
+++ app/src/main/java/com/example/crc_android/data/repository/FriendRepository.kt
@@ -0,0 +1,20 @@
+package com.example.crc_android.data.repository
+
+import com.example.crc_android.data.models.Data
+import com.example.crc_android.data.models.FriendUserData
+import com.example.crc_android.data.network.FriendApi
+import retrofit2.Response
+import javax.inject.Inject
+
+class FriendRepository @Inject constructor(
+    private val friendApi: FriendApi
+) {
+    suspend fun getFriendOne(token: String): Response<FriendUserData> =
+        friendApi.getFriendOne(token)
+
+    suspend fun getFriendTwo(token: String): Response<FriendUserData> =
+        friendApi.getFriendTwo(token)
+
+    suspend fun getFriendThree(token: String): Response<FriendUserData> =
+        friendApi.getFriendThree(token)
+}
