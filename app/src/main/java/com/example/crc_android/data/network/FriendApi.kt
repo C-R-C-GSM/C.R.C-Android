c1b35f3f3e15ddc217864b47fba2e16741b4df02
KyungMin <2004ckck@naver.com>
2021-08-12 23:13:55 +0900

    [UPDATE] @Query -> Header 로 변경

--- app/src/main/java/com/example/crc_android/data/network/FriendApi.kt
+++ app/src/main/java/com/example/crc_android/data/network/FriendApi.kt
@@ -4,22 +4,24 @@ import com.example.crc_android.data.models.Data
 import com.example.crc_android.data.models.FriendUserData
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface FriendApi {
 
     @GET("/check/one")
     suspend fun getFriendOne(
-        @Query("Token") token: String
+        @Header("Token") token: String
     ) : Response<FriendUserData>
 
     @GET("/check/two")
     suspend fun getFriendTwo(
-        @Query("Token") token: String
+        @Header("Token") token: String
     ): Response<FriendUserData>
 
     @GET("/check/three")
     suspend fun getFriendThree(
-        @Query("Token") token: String
+        @Header("Token") token: String
     ): Response<FriendUserData>
 }
