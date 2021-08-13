3fbb760e9bd6056acb66efb9e35d14e0b4c64009
SeungHyun <67039023+gusanans218@users.noreply.github.com>
2021-08-14 01:08:15 +0900

    [CREATE] 더보기

---
+++ app/src/main/java/com/example/crc_android/ViewmoreActivity.kt
@@ -0,0 +1,12 @@
+package com.example.crc_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewmoreActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_viewmore)
+        
+    }
+}
