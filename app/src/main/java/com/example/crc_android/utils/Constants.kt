b589083875c2766992474b8b37c108206f554ff8
KyungMin <2004ckck@naver.com>
2021-09-01 10:17:21 +0900

    [CREATE] base_url util 패키지에 생성

---
+++ app/src/main/java/com/example/crc_android/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.crc_android.utils
+
+class Constants {
+
+    companion object{
+        const val BASE_URL="http://ec2-3-34-189-53.ap-northeast-2.compute.amazonaws.com:3000/"
+    }
+}
