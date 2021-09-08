f219dec0add89cfdcbc4681dad0d6e6c07cd4e32
KyungMin <2004ckck@naver.com>
2021-09-08 22:36:53 +0900

    [UPDATE] base_url 변겨

--- app/src/main/java/com/example/crc_android/util/Util.kt
+++ app/src/main/java/com/example/crc_android/util/Util.kt
@@ -2,6 +2,6 @@ package com.example.crc_android.util
 
 class Util{
     companion object{
-        const val BASE_URL="ec2-3-35-81-230.ap-northeast-2.compute.amazonaws.com:3000/"
+        const val BASE_URL="http://ec2-3-34-189-53.ap-northeast-2.compute.amazonaws.com:3000/"
     }
 }
