5c25e9da09fad213c81942cf553118b2973ae1d8
KyungMin <2004ckck@naver.com>
2021-09-08 22:41:47 +0900

    [ADD] navigation direction 추가

--- app/src/main/java/com/example/crc_android/ui/review/review/ReviewFragment.kt
+++ app/src/main/java/com/example/crc_android/ui/review/review/ReviewFragment.kt
@@ -2,15 +2,18 @@ package com.example.crc_android.ui.review.review
 
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.crc_android.R
 import com.example.crc_android.adapter.ReviewCheckAdapter
 import com.example.crc_android.base.UtilityBase
 import com.example.crc_android.databinding.FragmentReviewBinding
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
 
+@AndroidEntryPoint
 class ReviewFragment : UtilityBase.BaseFragment<FragmentReviewBinding>(R.layout.fragment_review) {
 
     private val reviewViewModel: ReviewViewModel by viewModels()
@@ -23,35 +26,40 @@ class ReviewFragment : UtilityBase.BaseFragment<FragmentReviewBinding>(R.layout.
     }
 
     override fun FragmentReviewBinding.onCreateView() {
+        setAdapter()
+
         observeReviewCheck()
+        nextMovePage()
+
     }
 
     override fun FragmentReviewBinding.onViewCreated() {
-        setAdapter()
     }
 
     private fun setAdapter() {
         recyclerView.apply {
             this.adapter = reviewAdapter
             this.layoutManager =
-                LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+                LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
             this.setHasFixedSize(false)
         }
     }
 
     private fun observeReviewCheck() = lifecycleScope.launch {
+        reviewViewModel.getReviewCheck("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJrZXkiOjI1LCJyb2xlIjowLCJpYXQiOjE2MzEwOTkyNjYsImV4cCI6MTYzMTEwMjg2NiwiaXNzIjoiQy5SLkNfU0VSVkVSIn0.PIxL6ByfXP08cpC9cgOjO1azZM3IYiAR4IYxMlf6418")
         reviewViewModel.reviewItem.observe(viewLifecycleOwner, { data ->
-            reviewViewModel.getReviewCheck("")
 
-            // 값이 비어있지 않을 때
-            if (data.isNotEmpty()) {
-                // 값을 넣는다
-                reviewAdapter.setItemList(data)
-            }
+            reviewAdapter.setItemList(data)
+
 
         })
 
+    }
 
+    private fun nextMovePage() {
+        binding.nextImage.setOnClickListener {
+            findNavController().navigate(R.id.action_reviewFragment_to_reviewRegister)
+        }
     }
 
 }
