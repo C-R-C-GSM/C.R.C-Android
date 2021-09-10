c75d9c2348ac6b2fd150f84fb7c40d0b7e3a0765
ParkSangSun <appleasz1028102@gmail.com>
2021-09-11 00:12:03 +0900

    [ADD] 로그인 후 Token을 인코딩 후 DataStore에 저장

--- app/src/main/java/com/example/crc_android/util/App.kt
+++ app/src/main/java/com/example/crc_android/util/App.kt
@@ -5,7 +5,20 @@ import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class App : Application(){
+    private lateinit var dataStore : DataStoreModule
+
+    companion object {
+        private lateinit var crcApplication: App
+        fun getInstance() : App = crcApplication
+    }
+
+
     override fun onCreate(){
         super.onCreate()
+        crcApplication = this
+        dataStore = DataStoreModule(this)
     }
+
+    fun getDataStore() : DataStoreModule = dataStore
+
 }
