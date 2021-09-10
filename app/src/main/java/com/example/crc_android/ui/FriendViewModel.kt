429514ea199a68df50de27be10070d48b5348fd5
KyungMin <2004ckck@naver.com>
2021-09-10 15:33:42 +0900

    [ADD] data null 허용

--- app/src/main/java/com/example/crc_android/ui/FriendViewModel.kt
+++ app/src/main/java/com/example/crc_android/ui/FriendViewModel.kt
@@ -1,40 +1,44 @@
 package com.example.crc_android.ui
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.crc_android.data.models.Data
-import com.example.crc_android.data.models.FriendUserData
+import com.example.crc_android.data.network.model.Data
 import com.example.crc_android.data.repository.FriendRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
+import java.lang.Exception
 import javax.inject.Inject
 
+// TODO: dto는 똑같으니 one,two,three 함수를 하나로 모을까?
 @HiltViewModel
 class FriendViewModel @Inject constructor(
     private val friendRepository: FriendRepository
 ) : ViewModel() {
 
 
-    private var _friendEnterItem = MutableLiveData<List<Data>>()
+    private val _friendEnterItem = MutableLiveData<List<Data>>()
     val friendEnterItem: LiveData<List<Data>> get() = _friendEnterItem
 
 
-
-
     suspend fun getFriendOne(token: String, student_check: Int) = viewModelScope.launch {
-        friendRepository.getFriendOne(token).let { response ->
-            if (response.isSuccessful) {
 
-                when (student_check) {
-                    1 -> _friendEnterItem.value = response.body()?.data
-                    0 -> _friendEnterItem.value = response.body()?.data
+        try {
+            friendRepository.getFriendOne(token).let { response ->
+                if (response.isSuccessful) {
+
+                    when (student_check) {
+                        1 -> _friendEnterItem.value = response.body()?.data
+                        0 -> _friendEnterItem.value = response.body()?.data
+                    }
+
+
                 }
-
-
             }
+
+        } catch (e: Exception) {
+            Throwable(e)
 
         }
 
@@ -43,38 +47,42 @@ class FriendViewModel @Inject constructor(
 
 
     suspend fun getFriendTwo(token: String, student_check: Int) = viewModelScope.launch {
-        friendRepository.getFriendTwo(token).let { response ->
-            if (response.isSuccessful) {
+        try {
+            friendRepository.getFriendTwo(token).let { response ->
+                if (response.isSuccessful) {
 
-                when (student_check) {
-                    1 -> _friendEnterItem.value = response.body()?.data
-                    0 -> _friendEnterItem.value = response.body()?.data
+                    when (student_check) {
+                        1 -> _friendEnterItem.value = response.body()?.data
+                        0 -> _friendEnterItem.value = response.body()?.data
+                    }
+
+
                 }
 
-
             }
-
+        } catch (e: Exception) {
+            Throwable(e)
         }
 
     }
 
-    suspend fun getFriendThree(token: String,student_check:Int) = viewModelScope.launch {
-        friendRepository.getFriendThree(token).let { response ->
-            if (response.isSuccessful) {
+    suspend fun getFriendThree(token: String, student_check: Int) = viewModelScope.launch {
+        try {
+            friendRepository.getFriendThree(token).let { response ->
+                if (response.isSuccessful) {
 
-                when (student_check) {
-                    1 -> _friendEnterItem.value = response.body()?.data
-                    0 -> _friendEnterItem.value = response.body()?.data
+                    when (student_check) {
+                        1 -> _friendEnterItem.value = response.body()?.data
+                        0 -> _friendEnterItem.value = response.body()?.data
+                    }
                 }
-
-
             }
-
+        } catch (e: Exception) {
+            Throwable(e)
         }
 
+
     }
 
-    companion object {
-        const val TAG = "FriendViewModel"
-    }
+
 }
